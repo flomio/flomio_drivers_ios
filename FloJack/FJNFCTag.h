eeb9927c0e4f4b07ae989690f3e0379a8e2b22ce
John Bullard <john.m.bullard@gmail.com>
2013-02-06 23:06:20 -0500

    Adding code to parse NDEF records from NFC Forum Type 2 memory

--- FloJack/FJNFCTag.h
+++ FloJack/FJNFCTag.h
@@ -10,13 +10,45 @@
 #import "FJNDEFMessage.h"
 #import "FJNDEFRecord.h"
 
+typedef enum
+{
+    NFC_FORUM_TYPE_1 = 1,
+    NFC_FORUM_TYPE_2,
+    NFC_FORUM_TYPE_3,
+    NFC_FORUM_TYPE_4
+} nfc_forum_types_t;
+
 @interface FJNFCTag : NSObject
 
 @property(nonatomic, readonly) NSData *uid;
+@property(nonatomic, readonly) int  nfcForumType;
 @property(nonatomic, readonly) NSData *data;
 @property(nonatomic, readonly) FJNDEFMessage *ndefMessage;
 
 - (id)initWithUid:(NSData *)theUid;
-- (id)initWithUid:(NSData *)theUid andData:(NSData *)theData; 
+- (id)initWithUid:(NSData *)theUid andData:(NSData *)theData;
+
+
+/*
+ Collection of static methods for parsing NFC Forum Type 2 memory.
+ 
+ --READ--
+ -dynamic vs static memory
+ -r/w security
+ -cc bytes
+ -NDEF Message(s)
+ -counters
+ 
+ This will parse out Lock, Memory, NDEF, and other TLV headers.
+ 
+ 
+ --WRITE--
+ TODO
+ 
+ */
+#define NFC_FORUM_TYPE2_CC_LOC              13;
+
+- (FJNDEFMessage *)parseMemoryForNdefMessage;
+- (FJNDEFMessage *)type2ParseMemoryForNdefMessage;
 
 @end
--- FloJack/FJNFCTag.m
+++ FloJack/FJNFCTag.m
@@ -16,6 +16,7 @@
 @synthesize uid = _uid;
 @synthesize data = _data;
 @synthesize ndefMessage = _ndefMessage;
+@synthesize nfcForumType = _nfcForumType;
 
 
 // Designated initializer
@@ -24,6 +25,9 @@
     if(self) {
         _uid = [theUid copy];
         _data = [theData copy];
+        
+        _nfcForumType = NFC_FORUM_TYPE_2;
+        _ndefMessage = [self parseMemoryForNdefMessage];
         
         //NSArray *ndefRecords = [FJNDEFRecord parseData:_data andIgnoreMbMe:FALSE];
         //_ndefMessage = [[FJNDEFMessage alloc] initWithNdefRecords:ndefRecords];
@@ -34,6 +38,77 @@
 
 - (id)initWithUid:(NSData *)theUid;{
     return [self initWithUid:theUid andData:nil];
+}
+
+- (FJNDEFMessage *)parseMemoryForNdefMessage; {
+    switch (_nfcForumType) {
+        case NFC_FORUM_TYPE_1:
+        case NFC_FORUM_TYPE_2:
+            return [self type2ParseMemoryForNdefMessage];
+        case NFC_FORUM_TYPE_3:
+        case NFC_FORUM_TYPE_4:
+        default:
+            return [self type2ParseMemoryForNdefMessage];           
+        
+    }
+}
+
+- (FJNDEFMessage *)type2ParseMemoryForNdefMessage; {
+    if (_data == nil || _data.length < 48) {
+        return nil;
+    }    
+    
+    //check for errors in capability container
+    UInt8 ccMagicValue;
+    [_data getBytes:&ccMagicValue range:NSMakeRange(12, 1)];
+    
+    UInt8 ccVersion;
+    [_data getBytes:&ccVersion range:NSMakeRange(13, 1)];
+    
+//    UInt8 ccDataLen;
+//    [_data getBytes:&ccDataLen range:NSMakeRange(14, 1)];
+//    ccDataLen *= 8;
+    
+    UInt8 ccReadWriteAbility;
+    [_data getBytes:&ccReadWriteAbility range:NSMakeRange(15, 1)];
+    
+    if (ccMagicValue != 0xE1) {
+        NSLog(@"CC Magic Value not equal to 0xE1");
+        return nil;
+    }
+    else if (ccVersion != 0x10) {
+        NSLog(@"CC Version not euqal to 1.0");
+        return nil;        
+    }
+//    else if (( 0x0 | 4 >> ccReadWriteAbility) != 0x00) {
+//        NSLog(@"CC Read Access not available");
+//        return nil;        
+//    }
+    
+    
+    //find mandatory NDEF Message TLV
+    UInt8 ndefTLVType = 0x03;
+    NSData *ndefTLV = [[NSData alloc] initWithBytes:&ndefTLVType length:1];
+    NSRange ndefTLVRange = [_data rangeOfData:ndefTLV options:nil range:NSMakeRange(16, _data.length - 17)];
+    
+    if (ndefTLVRange.location == NSNotFound) {
+        NSLog(@"NDEF TLV not found");
+        return nil;
+    }
+    
+    UInt8 ndefTLVLength;
+    [_data getBytes:&ndefTLVLength range:NSMakeRange(ndefTLVRange.location + 1, 1)];
+    
+    if (ndefTLVLength > 0) {
+        NSData *ndefData = [_data subdataWithRange:NSMakeRange(ndefTLVRange.location + 2, ndefTLVLength)];
+        
+        NSArray *ndefRecords = [FJNDEFRecord parseData:ndefData andIgnoreMbMe:FALSE];
+        return [[FJNDEFMessage alloc] initWithNdefRecords:ndefRecords];
+    }
+    else {
+        NSLog(@"NDEF TLV found but length is zero");
+        return nil;
+    }
 }
 
 @end
