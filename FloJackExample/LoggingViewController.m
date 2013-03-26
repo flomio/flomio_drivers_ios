6e2e4c525c13b124d2e14b515cf3d74f12a4c007
John Bullard <john.m.bullard@gmail.com>
2013-03-26 10:57:11 -0400

    Cleaning up LoggingViewController

--- FloJackExample/LoggingViewController.m
+++ FloJackExample/LoggingViewController.m
@@ -35,49 +35,53 @@
     [super didReceiveMemoryWarning];
 }
 
+- (void)updateLogTextViewWithString:(NSString *)updateString {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        self.loggingTextView.text = [NSString stringWithFormat:@"%@ \n%@",self.loggingTextView.text, updateString];
+    });    
+}
+
 #pragma mark - FJNFCAdapterDelegate
 
 - (void)nfcAdapter:(FJNFCAdapter *)nfcAdapter didScanTag:(FJNFCTag *)theNfcTag {
-    dispatch_async(dispatch_get_main_queue(), ^{        
-        NSMutableString *textUpdate = [NSMutableString stringWithFormat:@"--Tag Found-- \nUID: %@ \nType: %d \nData: %@", [[theNfcTag uid] fj_asHexString], theNfcTag.nfcForumType,
-                                       [[theNfcTag data] fj_asHexString]];
-        
-        if (theNfcTag.ndefMessage != nil && theNfcTag.ndefMessage.ndefRecords != nil) {
-            for (FJNDEFRecord *ndefRecord in theNfcTag.ndefMessage.ndefRecords) {
-                [textUpdate appendString:@"\n\nNDEF Record Found"];
-                [textUpdate appendString:[NSString stringWithFormat:@"\nTNF: %d",ndefRecord.tnf]];
-                [textUpdate appendString:[NSString stringWithFormat:@"\nType: %@",[ndefRecord.type fj_asHexString]]];
-                [textUpdate appendString:[NSString stringWithFormat:@"\nPayload: %@ (%@)",[ndefRecord.payload fj_asHexString], [ndefRecord.payload fj_asASCIIStringEncoded]]];
-                
-                NSURL *url = [ndefRecord getUriFromPayload];
-                if (url != nil) {
-                    [textUpdate appendString:[NSString stringWithFormat:@"\nURI: %@", url.description]];
-                }
-                NSLog(@"TNF: %d Type: %@ Payload: %@", ndefRecord.tnf, [ndefRecord.type fj_asHexString], [ndefRecord.payload fj_asASCIIStringEncoded]);
+    NSMutableString *textUpdate = [NSMutableString stringWithFormat:@":::Tag Found \nUID: %@ \nType: %d \nData: %@", [[theNfcTag uid] fj_asHexString], theNfcTag.nfcForumType,
+                                   [[theNfcTag data] fj_asHexString]];
+    
+    if (theNfcTag.ndefMessage != nil && theNfcTag.ndefMessage.ndefRecords != nil) {
+        for (FJNDEFRecord *ndefRecord in theNfcTag.ndefMessage.ndefRecords) {
+            [textUpdate appendString:@"\n\nNDEF Record Found"];
+            [textUpdate appendString:[NSString stringWithFormat:@"\nTNF: %d",ndefRecord.tnf]];
+            [textUpdate appendString:[NSString stringWithFormat:@"\nType: %@",[ndefRecord.type fj_asHexString]]];
+            [textUpdate appendString:[NSString stringWithFormat:@"\nPayload: %@ (%@)",[ndefRecord.payload fj_asHexString], [ndefRecord.payload fj_asASCIIStringEncoded]]];
+            
+            NSURL *url = [ndefRecord getUriFromPayload];
+            if (url != nil) {
+                [textUpdate appendString:[NSString stringWithFormat:@"\nURI: %@", url.description]];
             }
         }
-        self.loggingTextView.text = [NSString stringWithFormat:@"%@ \n%@",self.loggingTextView.text, textUpdate];
-    });
+    }
+    
+    [self updateLogTextViewWithString:textUpdate];
 }
 
 - (void)nfcAdapter:(FJNFCAdapter *)nfcAdapter didHaveStatus:(NSInteger)statusCode {
-
-
+    NSMutableString *textUpdate = [NSMutableString stringWithFormat:@":::FloJack Status %d", statusCode];
+    [self updateLogTextViewWithString:textUpdate];
 }
 
 - (void)nfcAdapter:(FJNFCAdapter *)nfcAdapter didWriteTagAndStatusWas:(NSInteger)statusCode {
-
-
+    NSMutableString *textUpdate = [NSMutableString stringWithFormat:@":::FloJack Tag Write Status %d", statusCode];
+    [self updateLogTextViewWithString:textUpdate];
 }
 
 - (void)nfcAdapter:(FJNFCAdapter *)nfcAdapter didReceiveFirmwareVersion:(NSString*)theVersionNumber {
-
-
+    NSMutableString *textUpdate = [NSMutableString stringWithFormat:@":::FloJack Firmware Version %@", theVersionNumber];
+    [self updateLogTextViewWithString:textUpdate];
 }
 
 - (void)nfcAdapter:(FJNFCAdapter *)nfcAdapter didReceiveHardwareVersion:(NSString*)theVersionNumber; {
-
-
+    NSMutableString *textUpdate = [NSMutableString stringWithFormat:@":::FloJack Hardware Version %@", theVersionNumber];
+    [self updateLogTextViewWithString:textUpdate];
 }
 
 @end
