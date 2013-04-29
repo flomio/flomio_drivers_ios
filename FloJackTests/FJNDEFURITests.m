78dd35c98d3bc3f71e9a71207f38717c1459bd1f
John Bullard <john.m.bullard@gmail.com>
2013-04-29 12:50:30 -0400

    +2 unit tests for NDEF URI parser - (1) extra long payload test - (1) truncated NDEF message test

--- FloJackTests/FJNDEFURITests.m
+++ FloJackTests/FJNDEFURITests.m
@@ -16,7 +16,7 @@
 - (void)setUp
 {
     [super setUp];
-    self.ndefTestData = [[NSMutableArray alloc] initWithCapacity:2];
+    self.ndefTestData = [[NSMutableArray alloc] initWithCapacity:7];
     
     /**
      Test Dataset 1:
@@ -39,6 +39,7 @@
     NSData *ndefMessageDataT1 = [[NSData alloc] initWithBytes:bytesT1 length:bytesLengthT1];
     NSDictionary *dict = [NSDictionary dictionaryWithObjectsAndKeys:
                                 ndefMessageDataT1,      @"ndefData",
+                                ndefMessageDataT1,      @"ndefDataDecoded",
                                 ndefRecordCountT1,      @"ndefRecordCount",
                                 urlT1,                  @"url"
                                 , nil];
@@ -49,7 +50,6 @@
      TNF = TNF_WELL_KNOWN (0x01), RTD = RTD_URI (0x55)
      Payload = "http://www.flomio.com"
      */
-    // Type 2 tag memory
     int bytesLengthT2 = 15;
     UInt8 bytesT2[] = {
         0xd1, 0x01, 0x0b, 0x55,
@@ -64,11 +64,11 @@
     NSData *ndefMessageDataT2 = [[NSData alloc] initWithBytes:bytesT2 length:bytesLengthT2];
     NSDictionary *dictT2 = [NSDictionary dictionaryWithObjectsAndKeys:
                           ndefMessageDataT2,        @"ndefData",
+                          ndefMessageDataT2,        @"ndefDataDecoded",
                           ndefRecordCountT2,        @"ndefRecordCount",
                           urlT2,                    @"url"
                           , nil];
     [self.ndefTestData addObject:dictT2];
-    
     
     /**
      Test Dataset 3:
@@ -96,11 +96,11 @@
     NSData *ndefMessageDataT3 = [[NSData alloc] initWithBytes:bytesT3 length:bytesLengthT3];
     NSDictionary *dictT3 = [NSDictionary dictionaryWithObjectsAndKeys:
                             ndefMessageDataT3,        @"ndefData",
+                            ndefMessageDataT3,        @"ndefDataDecoded",
                             ndefRecordCountT3,        @"ndefRecordCount",
                             urlT3,                    @"url"
                             , nil];
     [self.ndefTestData addObject:dictT3];
-    
     
     /*
      Test Dataset 4:
@@ -120,6 +120,7 @@
     NSData *ndefMessageDataT4 = [[NSData alloc] initWithBytes:bytesT4 length:bytesLengthT4];
     NSDictionary *dictT4 = [NSDictionary dictionaryWithObjectsAndKeys:
                             ndefMessageDataT4,        @"ndefData",
+                            ndefMessageDataT4,        @"ndefDataDecoded",
                             ndefRecordCountT4,        @"ndefRecordCount",
                             urlT4,                    @"url"
                             , nil];
@@ -130,7 +131,6 @@
      TNF = TNF_WELL_KNOWN (0x01), RTD = RTD_URI (0x55)
      Payload = "sms:3055559334?body=NFC rocks my socks "    
      */
-    //TODO: tests still failing
     int bytesLengthT5 = 52; //44;
     UInt8 bytesT5[] = {
         0xd1, 0x01, 0x30, 0x55,
@@ -153,22 +153,146 @@
     NSData *ndefMessageDataT5 = [[NSData alloc] initWithBytes:bytesT5 length:bytesLengthT5];
     NSDictionary *dictT5 = [NSDictionary dictionaryWithObjectsAndKeys:
                             ndefMessageDataT5,        @"ndefData",
+                            ndefMessageDataT5,        @"ndefDataDecoded",
                             ndefRecordCountT5,        @"ndefRecordCount",
                             urlT5,                    @"url"
                             , nil];
     [self.ndefTestData addObject:dictT5];
+    
+    /**
+     Test Dataset 6:
+     TNF = TNF_WELL_KNOWN (0x01), RTD = RTD_URI (0x55)
+     Payload = "http://flomio.com/flomio/flomio/flomio/flomio/flomio/flomio/flomio/flomio/flomio/flomio/flomio/flomio/flomio/flomio/flomio/flomio/flomio/flomio/flomio/flomio/flomio"
+     */
+    int bytesLengthT6 = 162;
+    UInt8 bytesT6[] = {
+        0xD1, 0x01,
+        0x9E, 0x55, 0x03, 0x66,
+        0x6C, 0x6F, 0x6D, 0x69,
+        0x6F, 0x2E, 0x63, 0x6F,
+        0x6D, 0x2F, 0x66, 0x6C,
+        0x6F, 0x6D, 0x69, 0x6F,
+        0x2F, 0x66, 0x6C, 0x6F,
+        0x6D, 0x69, 0x6F, 0x2F,
+        0x66, 0x6C, 0x6F, 0x6D,
+        0x69, 0x6F, 0x2F, 0x66,
+        0x6C, 0x6F, 0x6D, 0x69,
+        0x6F, 0x2F, 0x66, 0x6C,
+        0x6F, 0x6D, 0x69, 0x6F,
+        0x2F, 0x66, 0x6C, 0x6F,
+        0x6D, 0x69, 0x6F, 0x2F,
+        0x66, 0x6C, 0x6F, 0x6D,
+        0x69, 0x6F, 0x2F, 0x66,
+        0x6C, 0x6F, 0x6D, 0x69,
+        0x6F, 0x2F, 0x66, 0x6C,
+        0x6F, 0x6D, 0x69, 0x6F,
+        0x2F, 0x66, 0x6C, 0x6F,
+        0x6D, 0x69, 0x6F, 0x2F,
+        0x66, 0x6C, 0x6F, 0x6D,
+        0x69, 0x6F, 0x2F, 0x66,
+        0x6C, 0x6F, 0x6D, 0x69,
+        0x6F, 0x2F, 0x66, 0x6C,
+        0x6F, 0x6D, 0x69, 0x6F,
+        0x2F, 0x66, 0x6C, 0x6F,
+        0x6D, 0x69, 0x6F, 0x2F,
+        0x66, 0x6C, 0x6F, 0x6D,
+        0x69, 0x6F, 0x2F, 0x66,
+        0x6C, 0x6F, 0x6D, 0x69,
+        0x6F, 0x2F, 0x66, 0x6C,
+        0x6F, 0x6D, 0x69, 0x6F,
+        0x2F, 0x66, 0x6C, 0x6F,
+        0x6D, 0x69, 0x6F, 0x2F,
+        0x66, 0x6C, 0x6F, 0x6D,
+        0x69, 0x6F, 0x2F, 0x66,
+        0x6C, 0x6F, 0x6D, 0x69,
+        0x6F, 0x2F, 0x66, 0x6C,
+        0x6F, 0x6D, 0x69, 0x6F
+    };
+    NSURL *urlT6 = [[NSURL alloc] initWithString:@"http://flomio.com/flomio/flomio/flomio/flomio/flomio/flomio/flomio/flomio/flomio/flomio/flomio/flomio/flomio/flomio/flomio/flomio/flomio/flomio/flomio/flomio/flomio"];
+    NSNumber *ndefRecordCountT6 = [NSNumber numberWithInt:1];
+    
+    
+    NSData *ndefMessageDataT6 = [[NSData alloc] initWithBytes:bytesT6 length:bytesLengthT6];
+    NSDictionary *dictT6 = [NSDictionary dictionaryWithObjectsAndKeys:
+                            ndefMessageDataT6,      @"ndefData",
+                            ndefMessageDataT6,        @"ndefDataDecoded",
+                            ndefRecordCountT6,      @"ndefRecordCount",
+                            urlT6,                  @"url"
+                            , nil];
+    [self.ndefTestData addObject:dictT6];
+    
+    /**
+     Test Dataset 7:
+     Incomplete NDEF Message (8 bytes truncated)
+     TNF = TNF_WELL_KNOWN (0x01), RTD = RTD_URI (0x55)
+     Payload = "http://flomio.com/flomio/flomio/flomio/flomio/flomio/flomio/flomio/flomio/flomio/flomio/flomio/flomio/flomio/flomio/flomio/flomio/flomio/flomio/flomio/flomio/flomio"
+     */
+    int bytesLengthT7 = 154;
+    UInt8 bytesT7[] = {
+        0xD1, 0x01,
+        0x9E, 0x55, 0x03, 0x66,
+        0x6C, 0x6F, 0x6D, 0x69,
+        0x6F, 0x2E, 0x63, 0x6F,
+        0x6D, 0x2F, 0x66, 0x6C,
+        0x6F, 0x6D, 0x69, 0x6F,
+        0x2F, 0x66, 0x6C, 0x6F,
+        0x6D, 0x69, 0x6F, 0x2F,
+        0x66, 0x6C, 0x6F, 0x6D,
+        0x69, 0x6F, 0x2F, 0x66,
+        0x6C, 0x6F, 0x6D, 0x69,
+        0x6F, 0x2F, 0x66, 0x6C,
+        0x6F, 0x6D, 0x69, 0x6F,
+        0x2F, 0x66, 0x6C, 0x6F,
+        0x6D, 0x69, 0x6F, 0x2F,
+        0x66, 0x6C, 0x6F, 0x6D,
+        0x69, 0x6F, 0x2F, 0x66,
+        0x6C, 0x6F, 0x6D, 0x69,
+        0x6F, 0x2F, 0x66, 0x6C,
+        0x6F, 0x6D, 0x69, 0x6F,
+        0x2F, 0x66, 0x6C, 0x6F,
+        0x6D, 0x69, 0x6F, 0x2F,
+        0x66, 0x6C, 0x6F, 0x6D,
+        0x69, 0x6F, 0x2F, 0x66,
+        0x6C, 0x6F, 0x6D, 0x69,
+        0x6F, 0x2F, 0x66, 0x6C,
+        0x6F, 0x6D, 0x69, 0x6F,
+        0x2F, 0x66, 0x6C, 0x6F,
+        0x6D, 0x69, 0x6F, 0x2F,
+        0x66, 0x6C, 0x6F, 0x6D,
+        0x69, 0x6F, 0x2F, 0x66,
+        0x6C, 0x6F, 0x6D, 0x69,
+        0x6F, 0x2F, 0x66, 0x6C,
+        0x6F, 0x6D, 0x69, 0x6F,
+        0x2F, 0x66, 0x6C, 0x6F,
+        0x6D, 0x69, 0x6F, 0x2F,
+        0x66, 0x6C, 0x6F, 0x6D,
+        0x69, 0x6F, 0x2F, 0x66,
+        0x6C, 0x6F, 0x6D, 0x69,
+    };
+    NSNumber *ndefRecordCountT7 = [NSNumber numberWithInt:0];
+    
+    NSData *ndefMessageDataT7 = [[NSData alloc] initWithBytes:bytesT7 length:bytesLengthT7];
+    NSDictionary *dictT7 = [NSDictionary dictionaryWithObjectsAndKeys:
+                            ndefMessageDataT7,      @"ndefData",
+                            nil,                    @"ndefDataDecoded",
+                            ndefRecordCountT7,      @"ndefRecordCount",
+                            nil,                    @"url"
+                            , nil];
+    [self.ndefTestData addObject:dictT7];
 }
 
 - (void)testFJNDEFMessageDataDecoding
 {
     for(id item in self.ndefTestData) {
         NSData *ndefData = [item objectForKey:@"ndefData"];
+        NSData *ndefDataDecoded = [item objectForKey:@"ndefDataDecoded"];
         NSNumber *ndefRecordCount = [item objectForKey:@"ndefRecordCount"];
         
         FJNDEFMessage *ndefMessage = [[FJNDEFMessage alloc] initWithByteBuffer:ndefData];
         NSData *ndefMessageDataDecoded = [ndefMessage asByteBuffer];
         
-        STAssertTrue(([ndefData isEqualToData:ndefMessageDataDecoded])
+        STAssertTrue(([ndefDataDecoded isEqualToData:ndefMessageDataDecoded] ||
+                      (ndefDataDecoded == nil && ndefMessageDataDecoded == nil))
                         , @"FJNDEFMessage decoded data is incorrect.");
         STAssertTrue((ndefMessage.ndefRecords.count == ndefRecordCount.intValue)
                         , @"FJNDEFMessage has incorrect number of NDEF records.");
@@ -179,6 +303,7 @@
 {
     for(id item in self.ndefTestData) {
         NSData *ndefData = [item objectForKey:@"ndefData"];
+        NSData *ndefDataDecoded = [item objectForKey:@"ndefDataDecoded"];
         NSNumber *ndefRecordCount = [item objectForKey:@"ndefRecordCount"];
         NSURL *url = [item objectForKey:@"url"];
         
@@ -187,9 +312,11 @@
         
         STAssertTrue(([ndefRecords count] == ndefRecordCount.intValue)
                      , @"FJNDEFRecord has incorrect number of NDEF Records.");
-        STAssertTrue(([ndefRecord.asByteBuffer isEqualToData:ndefData])
+        STAssertTrue(([ndefRecord.asByteBuffer isEqualToData:ndefDataDecoded] ||
+                      (ndefRecord.asByteBuffer == nil && ndefDataDecoded == nil))
                      , @"FJNDEFRecord decoded byte stream incorrect.");
-        STAssertTrue(([ndefRecord.getUriFromPayload isEqual:url])
+        STAssertTrue(([ndefRecord.getUriFromPayload isEqual:url] ||
+                      (ndefRecord == nil && url == nil))
                      , @"FJNDEFRecord decoded URI is incorrect.");
     }
 }
@@ -197,7 +324,7 @@
 - (void)testFJNDEFMessageDataEncoding
 {    
     for(id item in self.ndefTestData) {
-        NSData *ndefData = [item objectForKey:@"ndefData"];
+        NSData *ndefDataDecoded = [item objectForKey:@"ndefDataDecoded"];
         NSNumber *ndefRecordCount = [item objectForKey:@"ndefRecordCount"];
         NSURL *url = [item objectForKey:@"url"];
         
@@ -205,7 +332,8 @@
                 
         STAssertTrue((ndefMessage.ndefRecords.count == ndefRecordCount.intValue)
                      , @"FJNDEFMessage encoded incorrect number of NDEF Records.");
-        STAssertTrue(([ndefMessage.asByteBuffer isEqualToData:ndefData])
+        STAssertTrue(([ndefMessage.asByteBuffer isEqualToData:ndefDataDecoded] ||
+                      (ndefMessage.asByteBuffer == nil && ndefDataDecoded == nil))
                      , @"FJNDEFMessage encoded byte stream incorrect.");
         
     }
@@ -214,21 +342,25 @@
 - (void)testFJNDEFRecordDataEncoding
 {
     for(id item in self.ndefTestData) {
-        NSData *ndefData = [item objectForKey:@"ndefData"];
+        NSData *ndefDataDecoded = [item objectForKey:@"ndefDataDecoded"];
         NSURL *url = [item objectForKey:@"url"];
         
         FJNDEFRecord *ndefRecord = [FJNDEFRecord createURIWithString:url.absoluteString];
         
-        STAssertTrue(([ndefRecord.asByteBuffer isEqualToData:ndefData])
+        STAssertTrue(([ndefRecord.asByteBuffer isEqualToData:ndefDataDecoded] ||
+                      (ndefRecord.asByteBuffer == nil && ndefDataDecoded == nil))
                      , @"FJNDEFRecord decoded byte stream incorrect.");
-        STAssertTrue(([ndefRecord.getUriFromPayload isEqual:url])
+        STAssertTrue(([ndefRecord.getUriFromPayload isEqual:url] ||
+                      (ndefRecord == nil && url == nil))
                      , @"FJNDEFRecord decoded URI is incorrect.");
         
         ndefRecord = [FJNDEFRecord createURIWithURL:url];
         
-        STAssertTrue(([ndefRecord.asByteBuffer isEqualToData:ndefData])
+        STAssertTrue(([ndefRecord.asByteBuffer isEqualToData:ndefDataDecoded] ||
+                      (ndefRecord.asByteBuffer == nil && ndefDataDecoded == nil))
                      , @"FJNDEFRecord decoded byte stream incorrect.");
-        STAssertTrue(([ndefRecord.getUriFromPayload isEqual:url])
+        STAssertTrue(([ndefRecord.getUriFromPayload isEqual:url] ||
+                      (ndefRecord == nil && url == nil))
                      , @"FJNDEFRecord decoded URI is incorrect.");
     }
 }
