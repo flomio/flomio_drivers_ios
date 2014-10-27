5343914ff4e3208b068e10f2a5bb4fc032402800
Boris Polania <boris.polania@gmail.com>
2014-10-27 16:46:06 -0400

    SDK APP Basic

---
+++ SDKApp/IA_From/IA_From/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  IA_From
+//
+//  Created by Boris  on 10/27/14.
+//  Copyright (c) 2014 LLT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (nonatomic, strong) IBOutlet UILabel *resultLabel;
+
+
+@end
+
